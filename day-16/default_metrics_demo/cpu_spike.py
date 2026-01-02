ba0a01d4793dd223c02c436cac680134dab04861
cloud-devops-practitioner <srewithdevops@outlook.com>
2026-01-02 10:08:10 +0530

    Change CPU utilization from 80% to 60%

--- day-16/default_metrics_demo/cpu_spike.py
+++ day-16/default_metrics_demo/cpu_spike.py
@@ -22,5 +22,5 @@ def simulate_cpu_spike(duration=30, cpu_percent=80):
     print("CPU spike simulation completed.")
 
 if __name__ == '__main__':
-    # Simulate a CPU spike for 30 seconds with 80% CPU utilization
-    simulate_cpu_spike(duration=30, cpu_percent=80)
+    # Simulate a CPU spike for 30 seconds with 60% CPU utilization 
+    simulate_cpu_spike(duration=30, cpu_percent=60)
